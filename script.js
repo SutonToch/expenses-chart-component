c1bf8131f8b4ebb22613b9549bd057942d91a6bc
SutonToch <77286229+SutonToch@users.noreply.github.com>
2023-08-08 17:28:34 +0200

    ADD js in prep for next time

---
+++ script.js
@@ -0,0 +1,42 @@
+//import assertions currently do not work in firefox
+const data = [
+  {
+    "day": "mon",
+    "amount": 17.45
+  },
+  {
+    "day": "tue",
+    "amount": 34.91
+  },
+  {
+    "day": "wed",
+    "amount": 52.36
+  },
+  {
+    "day": "thu",
+    "amount": 31.07
+  },
+  {
+    "day": "fri",
+    "amount": 23.39
+  },
+  {
+    "day": "sat",
+    "amount": 43.28
+  },
+  {
+    "day": "sun",
+    "amount": 25.48
+  }];
+
+//put data into localStorage
+//query all expenses-day
+//query all expenses-column
+//query all day-of-the-week
+
+//render data from localStorage 
+//where the expenses-column height is relative to it's expense
+
+//add .expenses-column-highest-expense to column with highest expense
+
+//add .expenses-day-wrapper-show to .expenses-day-wrapper IF that column is hovered
